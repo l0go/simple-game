47f20ee6aaffa771a7358872630ce32ca0387e01
logo4poop <iamlogo@mail.com>
2019-06-23 11:36:13 -0400

    Gave player delta

--- Player.gd
+++ Player.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 
-export(int) var speed = 250
+export(int) var speed = 25000
 
 func _physics_process(delta):
     var motion = Vector2()
     if Input.is_action_pressed("ui_right"):
-        motion.x += speed
+        motion.x += speed * delta
     elif Input.is_action_pressed("ui_left"):
-        motion.x -= speed
+        motion.x -= speed * delta
     move_and_slide(motion)
 
