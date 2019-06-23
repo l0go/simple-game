c083e4fb98b4782dc3b7b24d83b082aa7cb2eb83
logo4poop <iamlogo@mail.com>
2019-06-23 10:06:57 -0400

    Add files via upload

---
+++ Player.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+export(int) var speed = 250
+
+func _physics_process(delta):
+	var motion = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		motion.x += speed
+	elif Input.is_action_pressed("ui_left"):
+		motion.x -= speed
+	move_and_slide(motion)
+
---
+++ brickspawner.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var scene = load("res://Brick.tscn")
+
+func _ready():
+	while 1 < 2:
+		yield(get_tree().create_timer(rand_range(3.5, 4)), "timeout")
+		drop()
+
+func drop():
+	var scene_instance = scene.instance()
+	scene_instance.set_name("scene")
+	
+	add_child(scene_instance)
